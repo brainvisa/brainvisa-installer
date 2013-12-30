0d618cee1c8523b78e9d04f2d8f80269caa958b2
denisri <denis.riviere.gm@gmail.com>
2013-12-30 16:55:53 +0000

    fixed linux32 system detection
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/brainvisa-installer/trunk@60223 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/installer/bvi_utils/system.py
+++ python/brainvisa/installer/bvi_utils/system.py
@@ -5,26 +5,26 @@ import platform
 
 
 class System(object):
-	"""Detect the platform with the BrainVISA Installer conventions."""
-	
-	Win32 = 'WIN32'
-	Win64 = 'WIN64'
-	Linux32 = 'LINUX32'
-	Linux64 = 'LINUX64'
-	MacOSX = 'OSX'
+    """Detect the platform with the BrainVISA Installer conventions."""
 
-	@staticmethod
-	def platform():
-		(system, node, release, version, machine, processor) = platform.uname() #pylint: disable=W0612
-		if system == 'Windows':
-			if machine == 'x86_64':
-				return System.Win64
-			return System.Win32
-		elif system == 'Linux':
-			if machine == 'x86_64':
-				return System.Linux64
-			elif machine == 'x86_32':
-				return System.Linux32
-		elif system == 'Darwin':
-			return System.MacOSX
-		return None
+    Win32 = 'WIN32'
+    Win64 = 'WIN64'
+    Linux32 = 'LINUX32'
+    Linux64 = 'LINUX64'
+    MacOSX = 'OSX'
+
+    @staticmethod
+    def platform():
+        (system, node, release, version, machine, processor) = platform.uname() #pylint: disable=W0612
+        if system == 'Windows':
+            if machine == 'x86_64':
+                return System.Win64
+            return System.Win32
+        elif system == 'Linux':
+            if machine == 'x86_64':
+                return System.Linux64
+            elif machine in ( 'x86_32', 'i386', 'i686' ):
+                return System.Linux32
+        elif system == 'Darwin':
+            return System.MacOSX
+        return None
