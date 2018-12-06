e078540d516bbfac625dfba15d95b6fdfeb74ddf
denisri <denis.riviere.gm@gmail.com>
2018-12-06 09:34:31 +0100

    fixes for python3

--- python/brainvisa/installer/bvi_utils/format.py
+++ python/brainvisa/installer/bvi_utils/format.py
@@ -3,35 +3,37 @@
 
 """Methods to format the string."""
 
+import six
+
 
 def ifw_version(string):
-	"Format to specify the version in QT"
-	res = string.strip()
-	res = res.replace(r'<<', r'<')
-	res = res.replace(r'>>', r'>')
-	res = res.replace(r';', r' ') 		
-	return res
+    "Format to specify the version in QT"
+    res = string.strip()
+    res = res.replace(r'<<', r'<')
+    res = res.replace(r'>>', r'>')
+    res = res.replace(r';', r' ')
+    return res
 
 
 def ifw_name(string):
-	res = string.strip()
-	res = res.lower()
-	res = res.replace('-', '_')
-	res = res.replace(' ', '_')
-	res = res.replace('.', '_')
-	return res
+    res = string.strip()
+    res = res.lower()
+    res = res.replace('-', '_')
+    res = res.replace(' ', '_')
+    res = res.replace('.', '_')
+    return res
 
 
 def xml_escape(string):
-	"Escape the invalid characters."
-	res = string.strip()
-	res = res.replace("&", "&amp;") # must be first! Else &quot; => &amp;quot; etc.
-	characters = {	
-		"\"" : "&quot;",
-		"'" : "&apos;",
-		"<" : "&lt;",
-		">" : "&gt;",
-		" " : ""}
-	for char, escape in characters.iteritems():
-		res = res.replace(char, escape)
-	return res
+    "Escape the invalid characters."
+    res = string.strip()
+    res = res.replace("&", "&amp;") # must be first! Else &quot; => &amp;quot; etc.
+    characters = {
+        "\"" : "&quot;",
+        "'" : "&apos;",
+        "<" : "&lt;",
+        ">" : "&gt;",
+        " " : ""}
+    for char, escape in six.iteritems(characters):
+        res = res.replace(char, escape)
+    return res
